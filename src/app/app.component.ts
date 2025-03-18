80dfad24043cd5e7866c25a65c37d74fcb0961f3
oArthur <arthursouzamaciel@gmail.com>
2025-03-17 23:14:16 -0300

    implementacao matomo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, LOCALE_ID } from '@angular/core';
-import {Router, RouterLink, RouterOutlet} from '@angular/router';
+import {Component, LOCALE_ID, OnInit} from '@angular/core';
+import {NavigationEnd, Router, RouterLink, RouterOutlet} from '@angular/router';
 import {registerLocaleData} from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
 
 registerLocaleData(localePt, 'pt-BR');
+declare let _paq: any;
 
 @Component({
   selector: 'app-root',
@@ -16,9 +17,20 @@ registerLocaleData(localePt, 'pt-BR');
     { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   constructor(private router: Router) {
+  }
+
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Atualiza a URL personalizada e registra a visualização da página
+        _paq.push(['setCustomUrl', event.urlAfterRedirects]);
+        _paq.push(['trackPageView']);
+      }
+    });
   }
   irParaHome() {
     this.router.navigate(['/']); // Redireciona para a página inicial
